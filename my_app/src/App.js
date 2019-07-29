32bf4806d3d52d7fa5f5d74cc7469aaf7f0d8e0b
Juicyjames <iceandjames@gmail.com>
2019-07-28 22:57:22 -0600

    Backdrop with z index added

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Toolbar from './components/Toolbar/Toolbar'
 import SidePanel from './components/SidePanel/SidePanel'
+import Backdrop from './components/Backdrop/Backdrop'
 
 function App() {
   return (
     <div style={{height: '100%'}}>
       <Toolbar />
       <SidePanel />
+      <Backdrop />
       <main style={{ marginTop: '64px' }}>      
         <p>This is the page content!</p>
       </main>
