2017e6d093649c45898a527401f793bd20039081
Juicyjames <iceandjames@gmail.com>
2019-07-28 22:12:54 -0600

    styled navigation and added flexbox with a spacer div

--- my_app/src/components/Toolbar/Toolbar.js
+++ my_app/src/components/Toolbar/Toolbar.js
@@ -7,6 +7,7 @@ const toolbar = props => (
         <nav className="toolbar__navigation">
             <div></div>
             <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+            <div className="spacer" />
             <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/">Projects</a></li>
