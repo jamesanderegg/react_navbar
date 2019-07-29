fcc494f7349cc45a4a46ce04c5acb3574cec5f65
Juicyjames <iceandjames@gmail.com>
2019-07-28 21:39:49 -0600

    starting with the toolbar

---
+++ my_app/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//function component
+const toolbar = props => (
+    <header>
+        <nav>
+            <div></div>
+            <div><a href="/">THE LOGO</a></div>
+        </nav>
+    </header>
+);
