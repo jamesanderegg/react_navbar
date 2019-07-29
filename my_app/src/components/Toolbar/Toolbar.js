bafadd3500ab7a7641e7bf37720ad985255b84e5
Juicyjames <iceandjames@gmail.com>
2019-07-28 21:57:49 -0600

    starting working on css for the toolbar. add classNames

--- my_app/src/components/Toolbar/Toolbar.js
+++ my_app/src/components/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Toolbar.css';
 
 //function component
 const toolbar = props => (
-    <header>
-        <nav>
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
             <div></div>
-            <div><a href="/">THE LOGO</a></div>
-            <div>
+            <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+            <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/">Projects</a></li>
                     <li><a href="/">Users</a></li>
